4f24ac024e6854c3c7feac466b4e5deebb855489
Ahmad Rahimie <newrehmi@gmail.com>
2016-06-22 18:54:00 +0800

    grammar fixes

--- app/Docs/View/introduction/about.php
+++ app/Docs/View/introduction/about.php
@@ -1,6 +1,6 @@
 <h1>About Exedra</h1>
 <p>Exedra is a nestful route-oriented PHP Micro-framework, capable of letting you design the map and routes of your application hierarchically, while being flexible, functional and contextual at the sametime, without much interfering with how your application is supposed to work.</p>
-<p>As a microframework, there aren't much services/components you can expect. It's an alternative to anyone who seeks much minimal design yet expandable, a container based microframework</p>
+<p>As a microframework, there aren't much services/components you can expect. It's an alternative to anyone who seeks much minimal design yet expandable, a container, route/resource oriented capable microframework.</p>
 <h2>URI oriented routing</h2>
 <p>Imagine about having list of APIs, most of them repetitively written in nearly similar patterns.</p>
 <pre><code>
@@ -37,4 +37,4 @@ $app->map['web']->any('/web')->group(function($group)
 });
 </code></pre>
 <h2>Is it REST capable?</h2>
-<p>Most of the convenient interfaces is already http verb oriented, the rest is designing the response, authentication, application design which is all up to developer themselves. Most of the examples shown are usually resourceful as you can see.</p>
+<p>Most of the convenient interfaces are already http verb oriented, the rest is designing the response, authentication, application design which is all up to developers themselves. Most of the examples shown are usually resourceful as you can see.</p>
